d435cba765fa3b4b4f5c3c39795666f05d17d01e
voan89 <voan@magnetstreet.com>
2013-08-30 11:58:22 -0500

    update1

---
+++ feedAggregate/src/main/java/com/vincentko/feedaggregate/controller/feedAggregate.java
@@ -0,0 +1,19 @@
+package com.vincentko.feedaggregate.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class feedAggregate {
+
+  @RequestMapping(value="/",method=RequestMethod.GET)
+  public ModelAndView home(HttpServletRequest request, HttpServletResponse response) {
+    
+    return new ModelAndView("home");
+  }
+} 
